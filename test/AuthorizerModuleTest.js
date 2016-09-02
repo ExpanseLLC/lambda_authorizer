02b2ac0ba958c4086a8ab83093874a8042b7abab
MeghanE <erickson.meghan@gmail.com>
2016-09-01 17:12:27 -0700

    finally figuring out stubbing in sinon

--- test/AuthorizerModuleTest.js
+++ test/AuthorizerModuleTest.js
@@ -2,7 +2,7 @@
  * Created by beezus on 8/31/16.
  */
 'use strict'
-const expect = require('chai').expect;
+const assert = require('chai').assert;
 const sinon = require('sinon');
 const td = require('testdouble');
 const AuthorizerModule= require('../lib/AuthorizerModule');
@@ -15,21 +15,50 @@ var event = {
     };
 
 var context = {};
+var googPrincipalId = "jenkypenky@gmail.com";
+var fbPrincipalId = "jenky@facebook.com";
+var amznPrincipalId = "jenkypenky@amazon.com";
+var idProviderFunction = 'callIdProvider';
+var buildPolicySpy;
 
-var googleModStub = {
-    callIdProvider() {
-        return "eb@gmail.com";
-    }
-};
 
-describe('authorizer unit tests', () => {
+describe('Authorizer Unit Tests on authorize()', () => {
     before('setup', () => {
-        console.info("who knows");
+        console.info("Stubs are Setting Up");
+        sinon.stub(authMod.googleMod, idProviderFunction, () => { return googPrincipalId; });
+        sinon.stub(authMod.facebookMod, idProviderFunction, () => { return fbPrincipalId; });
+        sinon.stub(authMod.amznMod, idProviderFunction, () => { return amznPrincipalId; });
+
+        // set up a spy for authMod.buildPolicy()
+        buildPolicySpy = sinon.spy(authMod, 'buildPolicy');
     });
 
-    it('authorizer should run', () => {
+    it('Authorizer authorize() flow in this case should never use facebook or amazon id providers', () => {
+        authMod.authorize(event, context);
+        assert(buildPolicySpy.called);
+        assert(buildPolicySpy.neverCalledWith(fbPrincipalId));
+        assert(buildPolicySpy.neverCalledWith(amznPrincipalId));
+    });
 
-        authMod.buildPolicy("foo@gmail.com");
+    it('Authorizer authorize() flow in this case should never use google or amazon id providers', () => {
+        authMod.googleMod.restore();
+        authMod.authorize(event, context);
+        assert(buildPolicySpy.called);
+        assert(buildPolicySpy.neverCalledWith(googPrincipalId));
+        assert(buildPolicySpy.neverCalledWith(amznPrincipalId));
+    });
+
+    // it('authorizer should run', () => {
+    //     authMod.buildPolicy("foo@gmail.com");
+    // });
+
+    afterEach(() => {
+        buildPolicySpy.called = false;
+    });
+
+    after('Run after each test', () => {
+        console.info("Cleaning up after each method . . . ");
+        buildPolicySpy.restore();
     });
 });
 
