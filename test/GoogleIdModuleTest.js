2c3b1b948a5306e75e99cf7d8d87140dd4293a51
MeghanE <erickson.meghan@gmail.com>
2016-09-05 14:34:05 -0700

    need to pull the principalId from the google token payload

---
+++ test/GoogleIdModuleTest.js
@@ -0,0 +1,9 @@
+/**
+ * Created by beezus on 9/5/16.
+ */
+'use strict';
+const assert = require('chai').assert;
+const sinon = require('sinon');
+const GoogleIdModule = require('../lib/GoogleIdModule');
+
+describe('GoogleIdModule Unit Tests', () => {});
