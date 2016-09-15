326aece74ff10580365b29e8a142e278b2c6fb27
MeghanE <erickson.meghan@gmail.com>
2016-09-15 12:26:17 -0700

    no longer returning null when lambda is executed

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ exports.handler = (event, context) => {
       .then( (policy) => {
         console.log('Success: Policy is '
             + JSON.stringify(policy));
+          context.succeed(policy);
       } )
       .catch((error) => {
         console.log('Error: error is '
             + JSON.stringify(error));
+          context.fail(error);
       });
 };
